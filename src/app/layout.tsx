ca59bdcc4ecd4be6145f72ea14c95904aa9ee51a
Victor Humberto <tormentavictor@gmail.com>
2026-06-03 00:37:24 -0300

    feat: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,29 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import { StoreProvider } from '@/components/StoreProvider';
+import type { Metadata } from "next";
+import "./globals.css";
+import { StoreProvider } from "@/components/StoreProvider";
 
 export const metadata: Metadata = {
-  title: 'D&D 5e — Gerenciador de Personagem',
-  description:
-    'Ficha digital de personagem para Dungeons & Dragons 5a Edição.',
+  title: "D&D 5e — Gerenciador de Personagem",
+  description: "Ficha digital de personagem para Dungeons & Dragons 5a Edição.",
+  icons: {
+    icon: "/favicon.jpg",
+  },
+  openGraph: {
+    title: "D&D 5e — Gerenciador de Personagem",
+    description: "Ficha digital de personagem para Dungeons & Dragons 5a Edição.",
+    url: "https://projeto-d-d-zeta.vercel.app/",
+    siteName: "D&D 5e — Gerenciador de Personagem",
+    images: [
+      {
+        url: "/favicon.jpg",
+        width: 1200,
+        height: 630,
+        alt: "D&D 5e — Gerenciador de Personagem",
+      },
+    ],
+    locale: "pt-BR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
